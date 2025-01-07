9356e93d8073d1a89ee1ceeab75d0c49ee603f9f
prasangeet <b23ch1033@iitj.ac.in>
2025-01-07 21:51:32 +0530

    complete

--- frontend/components/project_card.jsx
+++ frontend/components/project_card.jsx
@@ -8,6 +8,7 @@ function ProjectCard({ title, description, projectId }) {
   const handleProjectClick = () => {
     router.push(`/dashboard/projects/${projectId}`);
   };
+  
 
   return (
     <div 
